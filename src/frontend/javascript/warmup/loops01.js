ace3b31f71c860b7cafbdb66d5e9ccb9c4479bed
Saurav Sengupta <saurav.sengupta@oracle.com>
2025-11-27 12:54:04 +0530

    Modified loops01.js

--- src/frontend/javascript/warmup/loops01.js
+++ src/frontend/javascript/warmup/loops01.js
@@ -3,7 +3,11 @@
 for (let i=0; i<20; i++){
    console.log("Hello world - "+i);
 }
-
+console.log(" ");
 for (let i=0; i<=4; i++){
+   console.log("Hello world - "+i);
+}
+console.log(" ");
+for (let i=3; i<5; i++){
    console.log("Hello world - "+i);
 }
